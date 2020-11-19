464c4dd849a4ae5abeca8cde8948ffd0eafa7250
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-11-19 14:31:24 -0300

    Support Functions

--- support.c
+++ support.c
@@ -34,3 +34,28 @@ void prompt(void)
 		write(1, "$ ", 2);
 	}
 }
+/**
+ * _strstr - locates sub string
+ * @haystack: input one
+ * @needle: input two
+ * Return: sub string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
