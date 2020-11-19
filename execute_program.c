913f460f500850307936efb02e9d232f3c95e7c3
AmericoVespucio <70416497+AmericoVespucio@users.noreply.github.com>
2020-11-19 15:43:56 -0300

    Fixing Arguments and functionalities. Big Fixes

--- execute_program.c
+++ execute_program.c
@@ -1,28 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stddef.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "simple_shell.h"
 
 /**
  * execute_prog - Executes binary or builtin
  * @args: Argument buffer
  * @line: Line buffer
+ * @env: Environment
+ * @flow: Helps decide what code to run
  * Return: 1 if success
  */
 
-int execute_program(char **args, char *string)
+int execute_program(char **args, char *string, char **env, int flow)
 {
 	int status;
 
 	if (args[0] == NULL)
 		return (1);
-	if (builtin_checker(args, string) == 1)
+	if (flow == 1)
+	{
+	if (builtin_checker(args, string, env) == 1)
 		return (1);
+	}
 	status = launch_prog(args);
 	return (status);
 }
