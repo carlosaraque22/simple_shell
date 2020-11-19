b92ceddba523d83cd3ee479e36038d30bc40b62e
Carlos Araque <cararqueu@gmail.com>
2020-11-19 16:35:11 +0000

    push of main

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "simple_shell.h"
+
+/**
+ * main - Entry point for simple shell.
+ * @ac: number of arguments that have been entered.
+ * @av: array of character pointers.
+ * @env: Environment of simple shell.
+ * Return: 0.
+ */
+int main(int ac, char **av, char **env)
+{
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
+	shell(ac, av, env);
+}
