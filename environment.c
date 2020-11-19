c1b2a04b4ad94132708d4bc0fa45c44d8ebe9962
Carlos Araque <cararqueu@gmail.com>
2020-11-19 18:22:47 +0000

    add env

---
+++ environment.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * _getenv - gets env of input.
+ * @env: this is the input.
+ * Return: the env.
+ */
+
+char *_getenv(char *env)
+{
+	int i = 0, n = 0;
+	char *temp, *res;
+
+	while (environ[i] != NULL)
+	{
+		if (_strcmp(environ[i], env) == 0)
+			temp = environ[i];
+		i++;
+	}
+
+	while (temp[n] != '\0')
+	{
+		if (_strcmp(temp, env) == 0)
+			res = _strstr(temp, "/");
+		n++;
+	}
+	return (res);
+}
