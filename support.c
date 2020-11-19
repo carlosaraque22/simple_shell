f3f5a60639e79d4db055fee376497b3956c99dda
Carlos Araque <cararqueu@gmail.com>
2020-11-19 17:33:52 +0000

    add the strlen

--- support.c
+++ support.c
@@ -59,3 +59,21 @@ char *_strstr(char *haystack, char *needle)
 
 	return (NULL);
 }
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: this is the input char.
+ * Return: length of the input string
+ */
+int _strlen(char *c)
+{
+	int i = 1, sum = 0;
+	char pl = c[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = c[i++];
+	}
+	return (sum);
+}
