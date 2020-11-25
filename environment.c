225b43ea3f443f13d0c72a27e9af4b71d207c2d2
Carlos Araque <cararqueu@gmail.com>
2020-11-25 20:55:51 +0000

    fix

--- environment.c
+++ environment.c
@@ -13,18 +13,12 @@ char *_getenv(char *env)
 	int i = 0, n = 0;
 	char *temp, *res;
 
-	while (environ[i] != NULL)
+	while (i = 0; environ[i] != NULL; i++)
 	{
-		if (_strcmp(environ[i], env) == 0)
-			temp = environ[i];
-		i++;
+		n (_strlen(environ[i]));
+		write(1, environ[i], n);
+		write(1, "\n", 1);
 	}
 
-	while (temp[n] != '\0')
-	{
-		if (_strcmp(temp, env) == 0)
-			res = _strstr(temp, "/");
-		n++;
-	}
-	return (res);
+	return (0);
 }
